bf640303d710fd3650f812fed4c83a63135cce26
Konstantin Pavlov <kpavlov@users.noreply.github.com>
2022-02-06 22:54:16 +0200

    Optionally start secure server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import https from 'https'
+import fs from 'fs'
 import conversations from './conversations/router';
+
+dotenv.config()
 
 const app = express();
 
-const port = 8343;
+const port = process.env.PORT;
+const tls = process.env.TLS_ENABLED;
 
 app.get('/', (req, res) => {
     res.send('Twilio Mock API!');
@@ -11,12 +17,25 @@ app.get('/', (req, res) => {
 
 app.use('/v1/Conversations', conversations);
 
-const server = app.listen(port, () => {
-    console.info(`Twilio Mock server is listening on port ${port}`);
-})
-    .on('error', (err: Error) => {
-        console.error(`Can't start Twilio Mock server on port ${port}`, err);
-        throw err;
-    });
+let server;
+if (tls === 'true') {
+    const options = {
+        key: fs.readFileSync('./nginx/local.twilio.com.key'),
+        cert: fs.readFileSync('./nginx/local.twilio.com.crt'),
+    };
+    server = https.createServer(options, app)
+        .listen(port, () => {
+            console.info(`Secure Twilio Mock server is listening on port ${port}`);
+        })
+} else {
+    server = app.listen(port, () => {
+        console.info(`Twilio Mock server is listening on port ${port}`);
+    })
+}
+
+server.on('error', (err: Error) => {
+    console.error(`Can't start Twilio Mock server on port ${port}`, err);
+    throw err;
+});
 
 export default server;
