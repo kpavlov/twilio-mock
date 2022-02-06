0d02511e823b0e2ec8221b5684322e883c2e3f87
Konstantin Pavlov <kpavlov@users.noreply.github.com>
2022-02-06 15:31:43 +0200

    CircleCI: Build

--- cookies.sh
+++ cookies.sh
@@ -1,7 +1,14 @@
 yarn
-yarn proxy
 yarn build
+
+#docker build nginx -t twilio-proxy:latest
+#docker rm  --force  twilio-proxy
+#docker run -p 127.0.0.1:443:443 -d --rm --name twilio-proxy twilio-proxy:latest
+
+yarn proxy
 nohup yarn start:prod &
+sleep 1
+
 yarn test
 yarn proxy:stop
 killall node
