752f46beb0f0aa6a8548ffd5787b3d8befb22d24
Konstantin Pavlov <kpavlov@users.noreply.github.com>
2022-02-06 22:54:34 +0200

    Run tests with secure server

--- scripts/jestGlobalSetup.ts
+++ scripts/jestGlobalSetup.ts
@@ -4,5 +4,10 @@ dotenv.config() // This is needed in order to read .env file before tests
 
 // noinspection JSUnusedGlobalSymbols
 export default async () => {
+    if (process.env.TLS_ENABLED === 'true') {
+        console.log('Running tests with secure server')
+        const server = await require('../src/index')
+    }
+
     console.log('Ready to run tests')
 };
